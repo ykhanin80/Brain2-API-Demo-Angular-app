cc939668182f54377ee17535ac3ee9296a415a9c
Yevhen Khanin <yevhen.khanin@bizerba.com>
2025-08-23 10:02:30 -0400

    good coordinates static texts

--- src/app/data-maintenance/data-maintenance.ts
+++ src/app/data-maintenance/data-maintenance.ts
@@ -442,53 +442,32 @@ export class DataMaintenanceComponent implements OnInit {
         2:{x:206,y:60,width:40},
         3:{x:478,y:160,width:40},
         4:{x:620,y:160,width:40}, 
-        5:{x:424,y:224,width:40}, //
-        6:{x:498,y:224,width:40,align:'right'}, //  Total Fat %DV (e.g., 10%)
-        7:{x:424,y:248,width:40}, // Saturated Fat %DV
-        8:{x:498,y:248,width:40,align:'right'}, // Trans Fat
-        9:{x:424,y:274,width:70}, // Cholesterol amount
-        10:{x:10,y:44,width:40,align:'right'}, // Cholesterol %
-        11:{x:424,y:300,width:40}, // Sodium amount
-        12:{x:498,y:300,width:40,align:'right'},
-        13:{x:424,y:325,width:40}, // Total Carbohydrate grams
-        14:{x:498,y:325,width:40,align:'right'}, // Carbs %
-        15:{x:424,y:350,width:40}, // Dietary Fiber
-        16:{x:498,y:350,width:40,align:'right'},
-        17:{x:424,y:375,width:40}, // Total Sugars
-        18:{x:498,y:375,width:40,align:'right'}, // Includes Added Sugars
-        19:{x:424,y:401,width:40},
-        20:{x:424,y:427,width:40}, // Protein
-        21:{x:498,y:427,width:40}, // Vitamin D
-        22:{x:424,y:451,width:40}, // Calcium
-        23:{x:424,y:492,width:40}, // Iron
-        24:{x:424,y:517,width:40}, // Potassium
-        25:{x:424,y:543,width:40}, // Magnesium
-        26:{x:424,y:567,width:40}, // Zinc
-        27:{x:10,y:68,width:40}, // Selenium
-        28:{x:10,y:92,width:40}, // Copper
-        29:{x:10,y:116,width:40}, // Manganese
-        30:{x:10,y:140,width:40}, // Chromium
-        31:{x:10,y:184,width:40}, // Molybdenum
-        32:{x:10,y:208,width:40}, // Chloride
-        33:{x:10,y:230,width:40}, // Iodine
-        34:{x:10,y:250,width:40}, // Cobalt
-        35:{x:10,y:270,width:40}, // Vanadium
-        36:{x:10,y:290,width:40}, // Nickel
-        37:{x:10,y:310,width:40}, // Silicon
-        38:{x:10,y:330,width:40}, // Boron
-        39:{x:10,y:350,width:40}, // Lithium
-        40:{x:10,y:370,width:40}, // Arsenic
-        41:{x:10,y:390,width:40}, // Cadmium
-        42:{x:10,y:410,width:40}, // Antimony
-        43:{x:10,y:440,width:40}, // Thallium
-        44:{x:10,y:460,width:40}, // Lead
-        45:{x:10,y:480,width:40}, // Bismuth
-        46:{x:10,y:500,width:40}, // Polonium
-        47:{x:10,y:530,width:40}, // Astatine
-        48:{x:10,y:550,width:40}, // Radon
-        49:{x:10,y:570,width:40}, // Francium
-        50:{x:10,y:590,width:40} // Radium
-      }
+        5:{x:424,y:224,width:40},               27:{x:568,y:224,width:40},
+        6:{x:498,y:224,width:40,align:'right'}, 28:{x:635,y:224,width:40,align:'right'}, 
+        7:{x:424,y:248,width:40},               29:{x:568,y:248,width:40},
+        8:{x:498,y:248,width:40,align:'right'}, 30:{x:635,y:248,width:40,align:'right'},
+        9:{x:424,y:274,width:70},              10:{x:568,y:274,width:40,align:'right'},
+        11:{x:424,y:300,width:40},              31:{x:568,y:300,width:40},
+        12:{x:498,y:300,width:40,align:'right'}, 32:{x:635,y:300,width:40,align:'right'},
+        13:{x:424,y:325,width:40},              33:{x:568,y:325,width:40},
+        14:{x:498,y:325,width:40,align:'right'}, 34:{x:635,y:325,width:40,align:'right'},
+        15:{x:424,y:350,width:40},              35:{x:568,y:350,width:40},
+        16:{x:498,y:350,width:40,align:'right'}, 36:{x:635,y:350,width:40,align:'right'},
+        17:{x:424,y:375,width:40},              37:{x:568,y:375,width:40},
+        18:{x:498,y:375,width:40,align:'right'}, 38:{x:635,y:375,width:40,align:'right'},
+        19:{x:424,y:401,width:40},              39:{x:568,y:401,width:40},
+        20:{x:424,y:427,width:40,align:'right'}, 40:{x:568,y:427,width:40,align:'right'},
+        21:{x:498,y:427,width:40},              41:{x:635,y:427,width:40},
+        22:{x:424,y:451,width:40,align:'right'}, 42:{x:568,y:451,width:40,align:'right'},
+        23:{x:424,y:492,width:80},              43:{x:568,y:492,width:80},
+        24:{x:424,y:517,width:80},              44:{x:568,y:517,width:80},
+        25:{x:424,y:543,width:80},              45:{x:568,y:543,width:80},
+        26:{x:424,y:567,width:80},              46:{x:568,y:567,width:80},
+        47:{x:150,y:475,width:40},
+        48:{x:150,y:500,width:40},
+        49:{x:150,y:525,width:40}, 
+        50:{x:150,y:550,width:40}
+               }
     },
     dual: {
       // Approximated from provided dual panel image (update after real asset dropped in /public/labels/)
